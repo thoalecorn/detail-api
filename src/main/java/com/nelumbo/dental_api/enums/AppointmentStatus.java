1b710c5c001d3ad85041af0f1a8180f702db4a86
Thomas Cornejo <cuc20131002@mail.udes.edu.co>
2026-06-13 16:39:48 -0500

    feat: add enums Role, AppointmentStatus and HistoryEvent

---
+++ src/main/java/com/nelumbo/dental_api/enums/AppointmentStatus.java
@@ -0,0 +1,12 @@
+package com.nelumbo.dental_api.enums;
+
+public enum AppointmentStatus {
+    AGENDADA,
+    EN_CURSO,
+    ATENDIDA,
+    CANCELADA,
+    INASISTENCIA,
+    PENDIENTE_APROBACION,
+    RECHAZADA
+}
+
---
+++ src/main/java/com/nelumbo/dental_api/enums/HistoryEvent.java
@@ -0,0 +1,8 @@
+package com.nelumbo.dental_api.enums;
+
+public enum HistoryEvent {
+    ATENDIDA,
+    CANCELADA,
+    INASISTENCIA,
+    CARGO_POR_CANCELACION_TARDIA
+}
