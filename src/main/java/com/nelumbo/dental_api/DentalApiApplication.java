45dc662f9f2dbc9a45a04d34c8c65da41bb65e7f
Thomas Cornejo <cuc20131002@mail.udes.edu.co>
2026-06-13 16:00:43 -0500

    feat: initialize spring boot project

---
+++ src/main/java/com/nelumbo/dental_api/DentalApiApplication.java
@@ -0,0 +1,13 @@
+package com.nelumbo.dental_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelumbo/dental_api/DentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelumbo.dental_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
